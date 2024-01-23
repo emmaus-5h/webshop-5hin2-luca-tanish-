9843f4c628accb254f2e742fc115664c4644c85d
LucaVriends <115638247+LucaVriends@users.noreply.github.com>
2024-01-23 08:59:43 +0000

    meerdere producten

--- db/create.sql
+++ db/create.sql
@@ -8,6 +8,7 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2)
+  
 );
 
 
@@ -26,5 +27,7 @@ insert into products (name, description, code, price) values ('groen paard, The'
 insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
 insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
 insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
-
-
+insert into products (name, description, code, price) values ('kaaskoe', 'kaaskoe', '1253217321', 881);
+insert into products (name, description, code, price) values ('kleine kipje', 'kleine kip', '121834291321', 451);
+insert into products (name, description, code, price) values ('lelijk schaap', 'niet zo mooi schaap', '62332143', 122);
+insert into products (name, description, code, price) values ('normale koe', 'koe', '7321332', 17);
