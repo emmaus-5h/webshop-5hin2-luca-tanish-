da12bc7ea81ffa115a052de1dcf88944069a1812
Tanish-weebshit <93191306+Tanish-weebshit@users.noreply.github.com>
2024-01-26 13:30:08 +0000

    alleen nog aanrpoen om te sien

--- db/create.sql
+++ db/create.sql
@@ -7,8 +7,8 @@ CREATE TABLE products (
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2)
-  
+  price NUMERIC(10, 2),
+  kleur VARCHAR(15)
 );
 
 
@@ -21,13 +21,24 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('holy kow', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('schaap jesse', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('groen paard, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
-insert into products (name, description, code, price) values ('kaaskoe', 'kaaskoe', '1253217321', 881);
-insert into products (name, description, code, price) values ('kleine kipje', 'kleine kip', '121834291321', 451);
-insert into products (name, description, code, price) values ('lelijk schaap', 'niet zo mooi schaap', '62332143', 122);
-insert into products (name, description, code, price) values ('normale koe', 'koe', '7321332', 17);
+insert into products (name, description, code, price, kleur) values ('holy kow', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '1', 10.5, 'zwarr');
+insert into products (name, description, code, price, kleur) values ('schaap jesse', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '2', 11, 'rood');
+insert into products (name, description, code, price, kleur) values ('groen paard, The', 'Pellentesque at nulla. Suspendisse potenti.', '3', 13.5, 'geel');
+insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '4', 13.5,'lood');
+insert into products (name, description, code, price, kleur) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '5', 14, 'hool');
+insert into products (name, description, code, price, kleur) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '6', 14, 'geel');
+insert into products (name, description, code, price, kleur) values ('kaaskoe', 'kaaskoe', '7', 881, 'lololololkololololol');
+insert into products (name, description, code, price, kleur) values ('kleine kipje', 'kleine kip', '8', 451, 'lichybaae');
+insert into products (name, description, code, price, kleur) values ('lelijk schaap', 'niet zo mooi schaap', '9', 122, koolswar);
+insert into products (name, description, code, price, kleur) values ('normale koe', 'koe', '10', 17, 'groen');
+
+
+
+
+
+
+
+
+
+
+
