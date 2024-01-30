ad1474344d591cd8f61d78be2c1d853720c977f2
Tanish-weebshit <93191306+Tanish-weebshit@users.noreply.github.com>
2024-01-30 08:43:24 +0000

    kokomelun

--- db/create.sql
+++ db/create.sql
@@ -19,6 +19,12 @@ CREATE TABLE products (
   kleur VARCHAR(15)
 );
 
+CREATE TABLE vacht {
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  kleur varchar(15),
+  patroon VARCHAR(15)
+}
+
 
 --
 -- populate with data
@@ -32,7 +38,7 @@ CREATE TABLE products (
 insert into products (name, description, code, price, kleur) values ('holy kow', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '1', 10.5, 'zwarr');
 insert into products (name, description, code, price, kleur) values ('schaap jesse', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '2', 11, 'rood');
 insert into products (name, description, code, price, kleur) values ('groen paard, The', 'Pellentesque at nulla. Suspendisse potenti.', '3', 13.5, 'geel');
-insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '4', 13.5,'lood');
+insert into products (name, description, code, price, kleur) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '4', 13.5,'lood');
 insert into products (name, description, code, price, kleur) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '5', 14, 'hool');
 insert into products (name, description, code, price, kleur) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '6', 14, 'geel');
 insert into products (name, description, code, price, kleur) values ('kaaskoe', 'kaaskoe', '7', 881, 'lololololkololololol');
@@ -40,12 +46,16 @@ insert into products (name, description, code, price, kleur) values ('kleine kip
 insert into products (name, description, code, price, kleur) values ('lelijk schaap', 'niet zo mooi schaap', '9', 122, koolswar);
 insert into products (name, description, code, price, kleur) values ('normale koe', 'koe', '10', 17, 'groen');
 
-
-
-
-
-
-
+insert into vacht (id, kleur, patroon) values ('1', 'wit', 'gevlekt');
+insert into vacht (id, kleur, patroon) values ('2', 'bruin', 'gestipt');
+insert into vacht (id, kleur, patroon) values ('3', 'groen', 'gestreept');
+insert into vacht (id, kleur, patroon) values ('4', 'wit', 'gevlekt');
+insert into vacht (id, kleur, patroon) values ('5', 'zwart', 'albino');
+insert into vacht (id, kleur, patroon) values ('6', 'velvet rood', 'cheesecake');
+insert into vacht (id, kleur, patroon) values ('7', 'geel', 'gaten');
+insert into vacht (id, kleur, patroon) values ('8', 'wit', 'gevlekt');
+insert into vacht (id, kleur, patroon) values ('9', 'zwart', 'glad');
+insert into vacht (id, kleur, patroon) values ('10', 'wit', 'gevlekt');
 
 
 
