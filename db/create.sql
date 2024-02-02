96b54e9d1edcac9143514c0f34c4caf4398d3fc9
Tanish-weebshit <93191306+Tanish-weebshit@users.noreply.github.com>
2024-02-02 13:27:42 +0000

    db klaar?

--- db/create.sql
+++ db/create.sql
@@ -3,8 +3,6 @@
 --
 
 -- feedback GEE 30 jan 2024
-
---
 -- die tabellen zijn met elkaar verbonden via 1:n of n:m relaties
 -- bekijk eens de video uit de opdracht over tabellen toeveogen voor inspriratie
 -- een voorbeeld van een extra tabel die voor bijna alles wat je verkoopt kan is "merk"
@@ -38,22 +36,15 @@ CREATE TABLE patroon (
   patroon VARCHAR(15)
 );
 
+--insertions --->
 
---
--- populate with data
---
--- generared using
--- curl "https://api.mockaroo.com/api/910b6c20?count=100&key=90eac760" > seed.sql
---
--- want different data? check: https://www.mockaroo.com/910b6c20
---
 
 insert into products (name, description, code, price) values ('holy kow', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '1', 10.5);
 insert into products (name, description, code, price) values ('schaap jesse', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '2', 11);
 insert into products (name, description, code, price) values ('groen paard, The', 'Pellentesque at nulla. Suspendisse potenti.', '3', 13.5);
-insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '4', 13.5);
-insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '5', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '6', 14);
+insert into products (name, description, code, price) values ('zwaan', 'halve harrt', '4', 13.5);
+insert into products (name, description, code, price) values ('fazant', 'eieren stelen', '5', 14);
+insert into products (name, description, code, price) values ('egel', 'auw!', '6', 14);
 insert into products (name, description, code, price) values ('kaaskoe', 'kaaskoe', '7', 881);
 insert into products (name, description, code, price) values ('kleine kipje', 'kleine kip', '8', 451);
 insert into products (name, description, code, price) values ('lelijk schaap', 'niet zo mooi schaap', '9', 122);
