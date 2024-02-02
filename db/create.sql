1d31fb79819d69f036d848ca76d06de1c3ee87c5
Tanish-weebshit <93191306+Tanish-weebshit@users.noreply.github.com>
2024-02-02 13:06:40 +0000

    alle tabellen en eigenschappen

--- db/create.sql
+++ db/create.sql
@@ -50,18 +50,18 @@ CREATE TABLE patroon (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price, kleur) values ('holy kow', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '1', 10.5, 'zwarr');
-insert into products (name, description, code, price, kleur) values ('schaap jesse', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '2', 11, 'rood');
-insert into products (name, description, code, price, kleur) values ('groen paard, The', 'Pellentesque at nulla. Suspendisse potenti.', '3', 13.5, 'geel');
-insert into products (name, description, code, price, kleur) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '4', 13.5,'lood');
-insert into products (name, description, code, price, kleur) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '5', 14, 'hool');
-insert into products (name, description, code, price, kleur) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '6', 14, 'geel');
-insert into products (name, description, code, price, kleur) values ('kaaskoe', 'kaaskoe', '7', 881, 'lololololkololololol');
-insert into products (name, description, code, price, kleur) values ('kleine kipje', 'kleine kip', '8', 451, 'lichybaae');
-insert into products (name, description, code, price, kleur) values ('lelijk schaap', 'niet zo mooi schaap', '9', 122, 'koolswart');
-insert into products (name, description, code, price, kleur) values ('normale koe', 'koe', '10', 17, 'groen');
-insert into products (name, description, code, price, kleur) values ('kalf', 'kleine koe','11', 129, 'wit');
-insert into products (name, description, code, price, kleur) values ('eend', 'water vogel', '12', 2, 'groen');
+insert into products (name, description, code, price) values ('holy kow', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '1', 10.5);
+insert into products (name, description, code, price) values ('schaap jesse', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '2', 11);
+insert into products (name, description, code, price) values ('groen paard, The', 'Pellentesque at nulla. Suspendisse potenti.', '3', 13.5);
+insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '4', 13.5);
+insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '5', 14);
+insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '6', 14);
+insert into products (name, description, code, price) values ('kaaskoe', 'kaaskoe', '7', 881);
+insert into products (name, description, code, price) values ('kleine kipje', 'kleine kip', '8', 451);
+insert into products (name, description, code, price) values ('lelijk schaap', 'niet zo mooi schaap', '9', 122);
+insert into products (name, description, code, price) values ('normale koe', 'koe', '10', 17);
+insert into products (name, description, code, price) values ('kalf', 'kleine koe','11', 129);
+insert into products (name, description, code, price) values ('eend', 'water vogel', '12', 2);
 
 insert into Primairekleur (id, kleur) values ('1', 'wit');
 insert into Primairekleur (id, kleur) values ('2', 'zwart');
@@ -85,3 +85,8 @@ insert into soort (id, soort) values ('1', 'viervoeter');
 insert into soort (id, soort) values ('2', 'gevogelte');
 insert into soort (id, soort) values ('3', 'zeedier');
 
+insert into patroon (id, patroon) values('1', 'gevlekt');
+insert into patroon (id, patroon) values('2', 'gestreept');
+insert into patroon (id, patroon) values('3', 'gaten');
+insert into patroon (id, patroon) values('4', 'gestipt');
+insert into patroon (id, patroon) values('5', 'fade');
