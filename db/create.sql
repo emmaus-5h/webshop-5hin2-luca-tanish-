aa9c4fa29fc37a3066f1c7e40f59e5b1abe1a137
LucaVriends <115638247+LucaVriends@users.noreply.github.com>
2024-02-17 20:45:57 +0000

    simpele fixes en aanpassingen

--- db/create.sql
+++ db/create.sql
@@ -36,35 +36,34 @@ CREATE TABLE patroon (
 
 
 insert into products (name, description, code, price) values ('holy kow', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '1', 10.5);
-insert into products (name, description, code, price) values ('schaap jesse', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '2', 11);
-insert into products (name, description, code, price) values ('groen paard, The', 'Pellentesque at nulla. Suspendisse potenti.', '3', 13.5);
+insert into products (name, description, code, price) values ('schaap', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '2', 11);
+insert into products (name, description, code, price) values ('paard', 'Pellentesque at nulla. Suspendisse potenti.', '3', 13.5);
 insert into products (name, description, code, price) values ('zwaan', 'halve harrt', '4', 13.5);
-insert into products (name, description, code, price) values ('racistische fazant', 'eieren stelen', '5', 14);
+insert into products (name, description, code, price) values ('fazant', 'eieren stelen', '5', 14);
 insert into products (name, description, code, price) values ('egel', 'auw!', '6', 14);
 insert into products (name, description, code, price) values ('kaaskoe', 'kaaskoe', '7', 881);
-insert into products (name, description, code, price) values ('kleine kipje', 'kleine kip', '8', 451);
-insert into products (name, description, code, price) values ('lelijk schaap', 'niet zo mooi schaap', '9', 122);
-insert into products (name, description, code, price) values ('normale koe', 'koe', '10', 17);
+insert into products (name, description, code, price) values ('kip', 'kleine kip', '8', 451);
+insert into products (name, description, code, price) values ('zwart schaap', 'niet zo mooi schaap', '9', 122);
+insert into products (name, description, code, price) values ('koe', 'koe', '10', 17);
 insert into products (name, description, code, price) values ('kalf', 'kleine koe','11', 129);
 insert into products (name, description, code, price) values ('eend', 'water vogel', '12', 2);
 
-insert into kleur (id, PrimaireKleur) values('1', 'wit');
-insert into kleur (id, PrimaireKleur) values ('2', 'zwart');
-insert into kleur (id, PrimaireKleur) values ('3', 'bruin');
-insert into kleur (id, PrimaireKleur) values ('4', 'groen');
-insert into kleur (id, PrimaireKleur) values ('5', 'blauw');
-insert into kleur (id, PrimaireKleur) values ('6', 'grijs');
-insert into Kleur (id, PrimaireKleur) values ('7', 'geel');
-insert into kleur (id, PrimaireKleur) values ('8', 'rood');
-
-insert into Kleur (id, SecundaireKleur) values ('1', 'wit');
-insert into Kleur (id, SecundaireKleur) values ('2', 'zwart');
-insert into Kleur (id, SecundaireKleur) values ('3', 'bruin');
-insert into Kleur (id, SecundaireKleur) values ('4', 'groen');
-insert into Kleur (id, SecundaireKleur) values ('5', 'blauw');
-insert into Kleur (id, SecundaireKleur) values ('6', 'grijs');
-insert into Kleur (id, SecundaireKleur) values ('7', 'geel');
-insert into Kleur (id, SecundaireKleur) values ('8', 'rood');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('zwart', 'wit');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('zwart', 'bruin');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('zwart', 'grijs');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('zwart', 'oranje');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('wit', 'zwart');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('wit', 'bruin');
+insert into Kleur (PrimaireKleur,SecundaireKleur ) values ('wit', 'grijs');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('wit', 'oranje');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('grijs', 'wit');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('grijs', 'zwart');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('grijs', 'bruin');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('grijs', 'oranje');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('bruin', 'wit');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('bruin', 'zwart');
+insert into Kleur (PrimaireKleur,SecundaireKleur ) values ('bruin', 'grijs');
+insert into kleur (PrimaireKleur,SecundaireKleur ) values ('bruin', 'oranje');
 
 insert into soort (id, soort) values ('1', 'viervoeter');
 insert into soort (id, soort) values ('2', 'gevogelte');
